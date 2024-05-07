95af4f140bd7d587eb8abd26b7c9f05ac97afe4a
Sarah <shaworth@uw.edu>
2024-05-07 13:01:14 -0700

    adding header bar

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import {Header} from './HeaderBar.js';
 
+function App(props) {
+    return (
+        <div>
+            <header>
+                <Header/>
+            </header>
+        </div>
+
+    )
+}
+
+export default App
---
+++ src/HeaderBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export function Header(props) {
+    return (
+        <nav className="navbar navbar-expand-lg bottom-spacing">
+            <div className="container-fluid">
+                <h1 className="navbar-brand fs-1 fw-bold">UW Ride With Friends</h1>
+                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation"</div>>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarScroll">
+                    <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="index.html">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="activity.html">My Activity</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="rec-form.html">Create An Activity</a>
+                    </li>
+                    </ul>
+                </div> */}
+            </div>
+        </nav>
+    )
+}
