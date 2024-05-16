541b72855eb713986d054c0f8c208fa05903221f
Sarah <shaworth@uw.edu>
2024-05-15 20:22:00 -0700

    trying to make searchbar interactive

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SUGGESTED_DATA from '../data/suggest.json';
+
 
 export function SearchBar(props) {
+    const [input, setInput] = useState("");
+
+    const handleChange = (event) => {
+        const inputtedValue = event.target.value;
+        setInput(inputtedValue);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("searching", input);
+        setInput("");
+    }
+
     return (
         <div className="container-fluid">
-            <form className="d-flex" role="search" action="byName.html" method="GET">
+            <form className="d-flex" role="search" action="byName.html" method="GET" onSubmit={handleSubmit}>
                 <button type="button" className="btn btn-outline-dark">Filter By</button>
-                <input className="form-control m-2" type="search" placeholder="Search Activities" aria-label="Search"/>
+                <input className="form-control m-2" type="search" placeholder="Search Activities" aria-label="Search" value={input} onChange={handleChange}/>
                 <button className="btn btn-outline-dark" type="submit">Search</button>
             </form>
         </div>
