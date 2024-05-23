84b7f9575b6ad8b08be8390543d7019378c5d4f2
YaqiLu <yaqi0204@uw.edu>
2024-05-22 18:54:50 -0700

    Remove unused import

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import FilterDropdown from './FilterDropdown';
 
 export function SearchBar({ onFilterSelect, handleChange, value }) {
