356a024ce021f28138b2b1f30359549fe228c5ce
halle <halle15@uw.edu>
2024-05-29 01:58:45 -0700

    user authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './HeaderBar.js';
 import { HomePage } from './HomePage';
 import { MyActivity } from './MyActivityComponent.js';
 import { ActivityDetails } from './ActivityDetailsComponent.js';
 import { CreateActivity } from './CreateActivityComponent.js';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useNavigate, Outlet } from 'react-router-dom';
 import { SignUp } from './SignUp.js';
+import SignInPage from './SignInPage.js';
 import REGISTERED_ACTIVITIES from '../data/registered_activities.json';
+import ProfilePage from './ProfilePage.js';
+import DEFAULT_USERS from '../data/users.json';
 
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 function App(props) {
     // activities signed up for
     const [registeredActivities, setRegisteredActivities] = useState(REGISTERED_ACTIVITIES);
     // add activity to registered
     const addActivity = (activity) => {
-        setRegisteredActivities(registeredActivities => [...registeredActivities, activity]); 
+        setRegisteredActivities(registeredActivities => [...registeredActivities, activity]);
     };
+    const [currentUser, setCurrentUser] = useState(DEFAULT_USERS[0]) //default to null user
+
+    const navigateTo = useNavigate();
+
+    useEffect(() => {
+
+    const auth = getAuth();
+    onAuthStateChanged(auth, (firebaseUser) => {
+      if(firebaseUser) {
+        console.log("signing in as", firebaseUser.displayName)
+        console.log("firebaseuser: ", firebaseUser);
+        firebaseUser.userId = firebaseUser.uid;
+        firebaseUser.userName = firebaseUser.displayName;
+        firebaseUser.userImg = firebaseUser.photoURL || "/img/blank.jpg";
+        console.log("firebaseuser after adding fields :", firebaseUser)
+        setCurrentUser(firebaseUser);
+      }
+      else {
+        console.log("signed out");
+        setCurrentUser(DEFAULT_USERS[0]);
+      }
+    })
+
+
+  }, [])
+
+  const loginUser = (userObj) => {
+    console.log("logging in as", userObj.userName);
+    setCurrentUser(userObj);
+    
+    if(userObj.userId !== null){
+      navigateTo('/my-activity');
+    }
+  }
+
 
     return (
         <div>
-            <Header />
+            <Header currentUser={currentUser} />
 
             <main>
                 <Routes>
-\                   <Route index element={<HomePage />} />
+                    <Route index element={<HomePage />} />
                     <Route path="/activity" element={<ActivityDetails />} />
                     <Route path="/create-activity" element={<CreateActivity />} />
                     <Route path="/my-activity" element={<MyActivity registeredActivities={registeredActivities} />} />
                     <Route path="/activity/:name" element={<ActivityDetails />} />
+                    <Route path="/signin" element={<SignInPage currentUser={currentUser} loginCallback={loginUser} />} />
                     <Route path="/sign-up/:activity" element={<SignUp addActivity={addActivity} />} />
                     <Route path="*" element={<Navigate to="/" />} />
+                    <Route element={<ProtectedPage currentUser={currentUser} />}>
+                        <Route path="my-activity" element={<MyActivity currentUser={currentUser} />} />
+                        <Route path="my-activity" element={<Navigate to="/my-activity" />} />
+                        <Route path="profile" element={<ProfilePage currentUser={currentUser} />} />
+
+                    </Route>
                 </Routes>
             </main>
 
@@ -41,5 +87,12 @@ function App(props) {
 
     )
 }
-
+function ProtectedPage(props) {
+    if(props.currentUser.userId === null) {
+      return <Navigate to="/signin" />
+    }
+    else {
+      return <Outlet />
+    }
+  }
 export default App
---
+++ src/components/SignInPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { StyledFirebaseAuth } from 'react-firebaseui';
+import { EmailAuthProvider, getAuth } from 'firebase/auth'
+
+
+export default function SignInPage(props) {
+  const currentUser = props.currentUser;
+  const loginCallback = props.loginCallback;
+
+  const auth = getAuth();
+
+  if (currentUser.userId) { 
+    return <Navigate to="/my-activity" />
+  }
+  const configObj = {
+    signInOptions: [
+      {
+        provider: EmailAuthProvider.PROVIDER_ID,
+        requireDisplayName: true,
+      }
+    ],
+    signInFlow: 'popup',
+    callbacks: {
+      signInSuccessWithAuthResult: () => false 
+    },
+    credentialHelper: 'none'
+  }
+
+  return (
+    <div className="card bg-light">
+      <div className="container card-body">
+        <StyledFirebaseAuth uiConfig={configObj} firebaseAuth={auth} />
+
+      </div>
+    </div>
+  )
+}
